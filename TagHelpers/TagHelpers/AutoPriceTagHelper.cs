d78923f975e91210b1ad69faa31c2beba3437577
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-13 01:37:38 +0300

    class properties and attributes

---
+++ TagHelpers/TagHelpers/AutoPriceTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelpers.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("auto-price")]
+    public class AutoPriceTagHelper : TagHelper
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+        }
+    }
+}
