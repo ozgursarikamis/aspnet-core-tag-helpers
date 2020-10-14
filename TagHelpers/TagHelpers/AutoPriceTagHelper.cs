a0e4abcf1bcc565f4c8c3e253ecb0e9e4a33e6eb
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-14 14:11:57 +0300

    attribute selector : ends with...

--- TagHelpers/TagHelpers/AutoPriceTagHelper.cs
+++ TagHelpers/TagHelpers/AutoPriceTagHelper.cs
@@ -1,10 +1,9 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace TagHelpers.TagHelpers
 {
     // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
-    [HtmlTargetElement(Attributes = "[model='laser']")]
+    [HtmlTargetElement(Attributes = "[model $= 'er']")]
     public class AutoPriceTagHelper : TagHelper
     { 
         public string Make { get; set; }
