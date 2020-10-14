f9c936cdb7079b481624d8c55c49cd6d82d03d62
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-14 13:16:37 +0300

    Dependency Injection with Tag Helpers

---
+++ TagHelpers/TagHelpers/TimeSinceTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelpers.TagHelpers
+{
+    [HtmlTargetElement("time-since")]
+    public class TimeSinceTagHelper: TagHelper
+    {
+        public string CompareDateTime { get; set; }
+        public ITimeSinceService TimeSinceService { get; }
+
+        public TimeSinceTagHelper(ITimeSinceService timeSinceService)
+        {
+            TimeSinceService = timeSinceService;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.SetContent(TimeSinceService.TimeSince(Convert.ToDateTime(CompareDateTime)));
+        }
+    }
+}
---
+++ TagHelpers/TimeSinceService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TagHelpers
+{
+    public interface ITimeSinceService
+    {
+        string TimeSince(DateTime dateTime);
+    }
+
+    public class TimeSinceService: ITimeSinceService
+    {
+        public string TimeSince(DateTime dateTime)
+        {
+            var timeSpan = DateTime.Now.Subtract(dateTime);
+            return PeriodOfTimeOutput(timeSpan);
+        }
+
+        /// <summary>
+        /// from: http://stackoverflow.com/questions/3383464/is-there-a-smarter-way-to-generate-time-since-with-a-datetime-objects
+        /// </summary>
+        /// <param name="tspan"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        private static string PeriodOfTimeOutput(TimeSpan tspan, int level = 0)
+        {
+            var howLongAgo = "ago";
+            if (level >= 2) return howLongAgo;
+            if (tspan.Days > 1)
+                howLongAgo = $"{tspan.Days} Days ago";
+            else if (tspan.Days == 1)
+                howLongAgo =
+                    $"1 Day {PeriodOfTimeOutput(new TimeSpan(tspan.Hours, tspan.Minutes, tspan.Seconds), level + 1)}";
+            else if (tspan.Hours >= 1)
+                howLongAgo =
+                    $"{tspan.Hours} {((tspan.Hours > 1) ? "Hours" : "Hour")} {PeriodOfTimeOutput(new TimeSpan(0, tspan.Minutes, tspan.Seconds), level + 1)}";
+            else if (tspan.Minutes >= 1)
+                howLongAgo =
+                    $"{tspan.Minutes} {((tspan.Minutes > 1) ? "Minutes" : "Minute")} {PeriodOfTimeOutput(new TimeSpan(0, 0, tspan.Seconds), level + 1)}";
+            else if (tspan.Seconds >= 1)
+                howLongAgo = $"{tspan.Seconds} {((tspan.Seconds > 1) ? "Seconds" : "Second")} ago";
+            return howLongAgo;
+        }
+    }
+}
