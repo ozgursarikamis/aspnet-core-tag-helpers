0ac3be0f17529d9f43f0bd13083e718c62ed5b0d
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-14 15:06:00 +0300

    Attribute selectors: multiple filters

--- TagHelpers/TagHelpers/AutoPriceTagHelper.cs
+++ TagHelpers/TagHelpers/AutoPriceTagHelper.cs
@@ -3,7 +3,8 @@
 namespace TagHelpers.TagHelpers
 {
     // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
-    [HtmlTargetElement(Attributes = "[model ^= 'must']")]
+    [HtmlTargetElement(Attributes = "[make ^= 't']")]
+    [HtmlTargetElement(Attributes = "[make ^= 'f'],[model $= 'er']")]
     public class AutoPriceTagHelper : TagHelper
     { 
         public string Make { get; set; }
