19211c6b9bbc9c3b96d4bd258414f1b6265946bc
ozgursarikamis <ozgursarikamis@gmail.com>
2020-10-14 12:57:52 +0300

    Generating HTML content with TagHelpers

---
+++ TagHelpers/TagHelpers/TabItemTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelpers.TagHelpers
+{
+    [RestrictChildren("tab-item")]
+    public class TabItemTagHelper : TagHelper
+    {
+        public string Title { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "li";
+            var str = $"<a class='nav-link' href='#'>{Title}</a>";
+            output.Content.SetHtmlContent(str);
+        }
+    }
+}
---
+++ TagHelpers/TagHelpers/TabTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelpers.TagHelpers
+{
+    [RestrictChildren("tab-item")]
+    public class TabTagHelper: TagHelper
+    {
+        [HtmlAttributeName("active-page")]
+        public string ActivePage { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.PreContent.SetHtmlContent("<h3>Silicon Valley Code Camp</h3><ul class='nav nav-tabs'>");
+            output.PostContent.SetHtmlContent("</ul>");
+            output.Attributes.Add("class", "container");
+        }
+    }
+}
